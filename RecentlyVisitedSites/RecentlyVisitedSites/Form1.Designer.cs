f33dcde370a60e95106a8830e7e21ebfe8912d2c
JoseGuadron <49799419+JoseGuadron@users.noreply.github.com>
2021-01-04 02:02:43 -0600

    Add files via upload

---
+++ RecentlyVisitedSites/RecentlyVisitedSites/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace RecentlyVisitedSites
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.link1 = new System.Windows.Forms.LinkLabel();
+            this.link2 = new System.Windows.Forms.LinkLabel();
+            this.link3 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // link1
+            // 
+            this.link1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.link1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.link1.Location = new System.Drawing.Point(28, 9);
+            this.link1.Name = "link1";
+            this.link1.Size = new System.Drawing.Size(224, 86);
+            this.link1.TabIndex = 0;
+            this.link1.TabStop = true;
+            this.link1.Text = "Facebook";
+            this.link1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.link1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.link1_LinkClicked);
+            this.link1.MouseLeave += new System.EventHandler(this.link1_MouseLeave);
+            this.link1.MouseHover += new System.EventHandler(this.link1_MouseHover);
+            // 
+            // link2
+            // 
+            this.link2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.link2.Location = new System.Drawing.Point(28, 104);
+            this.link2.Name = "link2";
+            this.link2.Size = new System.Drawing.Size(224, 86);
+            this.link2.TabIndex = 1;
+            this.link2.TabStop = true;
+            this.link2.Text = "Instagram";
+            this.link2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.link2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.link2_LinkClicked);
+            this.link2.MouseLeave += new System.EventHandler(this.link2_MouseLeave);
+            this.link2.MouseHover += new System.EventHandler(this.link2_MouseHover);
+            // 
+            // link3
+            // 
+            this.link3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.link3.Location = new System.Drawing.Point(28, 202);
+            this.link3.Name = "link3";
+            this.link3.Size = new System.Drawing.Size(224, 89);
+            this.link3.TabIndex = 2;
+            this.link3.TabStop = true;
+            this.link3.Text = "Twitter";
+            this.link3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.link3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.link3_LinkClicked);
+            this.link3.MouseLeave += new System.EventHandler(this.link3_MouseLeave);
+            this.link3.MouseHover += new System.EventHandler(this.link3_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 311);
+            this.Controls.Add(this.link1);
+            this.Controls.Add(this.link3);
+            this.Controls.Add(this.link2);
+            this.Name = "Form1";
+            this.Text = "RecentlyVisitedSites";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.LinkLabel link1;
+        private System.Windows.Forms.LinkLabel link2;
+        private System.Windows.Forms.LinkLabel link3;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ RecentlyVisitedSites/RecentlyVisitedSites/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecentlyVisitedSites
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void link1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.link2.LinkVisited = true;
+            System.Diagnostics.Process.Start("http://www.facebook.com");
+        }
+
+        private void link2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.link2.LinkVisited = true;
+            System.Diagnostics.Process.Start("http://www.instagram.com");
+        }
+
+        private void link3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.link3.LinkVisited = true;
+            System.Diagnostics.Process.Start("http://www.twitter.com");
+        }
+
+        private void link1_MouseHover(object sender, EventArgs e)
+        {
+            link1.Text = "Facebook is a popular free social networking website that allows registered users to create profiles, upload photos and video, send messages and keep in touch with friends, family and colleagues.";
+        }
+
+        private void link2_MouseHover(object sender, EventArgs e)
+        {
+            link2.Text = "Instagram is a photo and video-sharing social networking service owned by Facebook, Inc. The app allows users to upload photos and videos to the service, which can be edited with various filters, and organized with tags and location information.";
+        }
+
+        private void link3_MouseHover(object sender, EventArgs e)
+        {
+            link3.Text = "Twitter is an online news and social networking service on which users post and interact with messages known as tweets. Tweets were originally restricted to 140 characters";
+        }
+
+        private void link1_MouseLeave(object sender, EventArgs e)
+        {
+            link1.Text = "Facebook";
+        }
+
+        private void link2_MouseLeave(object sender, EventArgs e)
+        {
+            link2.Text = "Instagram";
+        }
+
+        private void link3_MouseLeave(object sender, EventArgs e)
+        {
+            link3.Text = "Twitter";
+        }
+
+    }
+}
